6608b9ad7d8801b5b1dd8fde3151a32856b21a81
AnmisaReddyM <82710101+AnmisaReddyM@users.noreply.github.com>
2021-04-26 11:01:54 +0530

    Add files via upload

---
+++ arraylisteg.java
@@ -0,0 +1,19 @@
+package arraylist;
+import java.util.*;
+
+public class arraylisteg {
+    public static void main(String args[]){
+        //creating a list
+        //list in java provides the facility to maintain the ordered collection
+        List<String>list=new ArrayList<String>();
+        //Adding elements in the list
+        list.add("Mango");
+        list.add("orange");
+        list.add("grapes");
+        list.add("Banana");
+        list.add("Apple");
+        //lterating the list elements using for-each loop
+        for(String fruit:list)
+            System.out.println(fruit);
+    }
+}
